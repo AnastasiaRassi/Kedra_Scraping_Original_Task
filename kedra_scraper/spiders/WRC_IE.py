a14704eedae6fb297e0670b427f2bf70a799f401
Anastasia Al Rassi <anastasiarassi2@gmail.com>
2026-09-02 13:32:21 +0300

    fixed spider

---
+++ kedra_scraper/spiders/WRC_IE.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class WRC_IE_Spider(scrapy.Spider):
+    name = "WRC_IE"
+    allowed_domains = ["workplacerelations.ie"]
+    start_urls = ["https://www.workplacerelations.ie/en/cases/"]
+
+    def parse(self, response):
+        pass
